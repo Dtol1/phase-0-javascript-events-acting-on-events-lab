caf75383d29c9ef9c7af9e87d67c9cfd416e9d01
Davis Toler <tolerdavis0@gmail.com>
2022-11-01 12:50:25 -0500

    commit tree 925c32a57c335933dc052b1fccf43f4c893ea3fc

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "blue"; 
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 5}px`;
+    }
+}
+
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0){
+        dodger.style.left = `${left + 5}px`;
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    // if (e.key === "ArrowLeft") {
+    //   moveDodgerLeft();
+    // }
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+        
+    } else { (e.key === "ArrowRight")
+        moveDodgerRight(); 
+
+        
+    }
+});
+
+// document.addEventListener("keydown", function (e){
+//     if (e.key === "ArrowRight"){
+//         moveDodgerRight();
+//     }
+// })
